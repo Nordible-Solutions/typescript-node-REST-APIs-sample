4bba3430437eba29f15c34acb242246398291ee8
xameeramir <zameeramir@live.com>
2018-09-04 14:58:40 +0530

    compiled changes

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
-const PORT = 3000;
+const PORT = process.env.PORT;
 app_1.default.listen(PORT, () => {
     console.log(`Battles API server running on ${PORT}`);
 });
